f70f36dd174a4b1e5ce0be87d213f5cb25367c63
Vellazxy <vellazxy@icloud.com>
2020-05-05 19:59:26 -0400

    draft

---
+++ code.js
@@ -0,0 +1,11 @@
+var p1 = document.getElementById("p1")
+
+p1.onmousemove = function(event){
+	p1.style.opacity = 1;
+}
+
+var p3 = document.getElementById("p3")
+
+p3.onmousemove = function(event){
+	p3.style.opacity = 1;
+}
