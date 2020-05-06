d71786287fb9883ade3367aaab7412819c67e1c4
Vellazxy <vellazxy@icloud.com>
2020-05-05 20:22:17 -0400

    1

--- code.js
+++ code.js
@@ -9,3 +9,9 @@ var p3 = document.getElementById("p3")
 p3.onmousemove = function(event){
 	p3.style.opacity = 1;
 }
+
+var p2 = document.getElementById("light")
+
+p2.onmousemove = function(event){
+	p2[0].style.opacity = 1;
+}
