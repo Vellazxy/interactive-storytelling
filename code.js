5e235e416c6109e146aa255e55391e4bedf9aa17
Vellazxy <vellazxy@icloud.com>
2020-05-05 20:29:19 -0400

    2

--- code.js
+++ code.js
@@ -4,14 +4,44 @@ p1.onmousemove = function(event){
 	p1.style.opacity = 1;
 }
 
+var p2 = document.getElementById("p2")
+
+p2.onmousemove = function(event){
+	p2.style.opacity = 1;
+}
+
 var p3 = document.getElementById("p3")
 
 p3.onmousemove = function(event){
 	p3.style.opacity = 1;
 }
 
-var p2 = document.getElementById("light")
+var p4 = document.getElementById("p4")
+
+p4.onmousemove = function(event){
+	p4.style.opacity = 1;
+}
+
+var p5 = document.getElementById("p5")
+
+p5.onmousemove = function(event){
+	p5.style.opacity = 1;
+}
+
+var p6 = document.getElementById("p6")
+
+p6.onmousemove = function(event){
+	p6.style.opacity = 1;
+}
+
+var p7 = document.getElementById("p7")
+
+p7.onmousemove = function(event){
+	p7.style.opacity = 1;
+}
+
+var p8 = document.getElementById("p8")
 
 p2.onmousemove = function(event){
-	p2[0].style.opacity = 1;
+	p8.style.opacity = 1;
 }
