1b4c3bd1b87068f452022a60f19422a809fe1a90
Vellazxy <vellazxy@icloud.com>
2020-05-07 18:03:44 -0400

    almost

--- code.js
+++ code.js
@@ -1,13 +1,13 @@
 var p1 = document.getElementById("p1")
 
 p1.onmousemove = function(event){
-	p1.style.opacity = 1;
+	p1.classList.add("show");
 }
 
 var p2 = document.getElementById("p2")
 
 p2.onmousemove = function(event){
-	p2.style.opacity = 1;
+	p2.classList.add("show");
 }
 
 var p3 = document.getElementById("p3")
@@ -16,16 +16,10 @@ p3.onmousemove = function(event){
 	p3.style.opacity = 1;
 }
 
-var p4 = document.getElementById("p4")
+var p4 = document.getElementById("container2")
 
 p4.onmousemove = function(event){
 	p4.style.opacity = 1;
-}
-
-var p5 = document.getElementById("p5")
-
-p5.onmousemove = function(event){
-	p5.style.opacity = 1;
 }
 
 var p6 = document.getElementById("p6")
@@ -40,8 +34,122 @@ p7.onmousemove = function(event){
 	p7.style.opacity = 1;
 }
 
+var s1 = document.getElementById("s1")
+
+s1.onmousemove = function(event){
+	s1.style.opacity = 1;
+}
+
 var p8 = document.getElementById("p8")
 
-p2.onmousemove = function(event){
+p8.onmousemove = function(event){
 	p8.style.opacity = 1;
+}
+
+var p9 = document.getElementById("p9")
+
+p9.onmousemove = function(event){
+	p9.style.opacity = 1;
+}
+
+var p10 = document.getElementById("p10")
+
+p10.onmousemove = function(event){
+	p10.style.opacity = 1;
+}
+
+var p11 = document.getElementById("p11")
+
+p11.onmousemove = function(event){
+	p11.style.opacity = 1;
+}
+
+var p12 = document.getElementById("p12")
+
+p12.onmousemove = function(event){
+	p12.style.opacity = 1;
+}
+
+var s2 = document.getElementById("s2")
+
+s2.onmousemove = function(event){
+	s2.style.opacity = 1;
+}
+
+var p13 = document.getElementById("p13")
+
+p13.onmousemove = function(event){
+	p13.style.opacity = 1;
+}
+
+var p14 = document.getElementById("p14")
+
+p14.onmousemove = function(event){
+	p14.style.opacity = 1;
+}
+
+var p15 = document.getElementById("p15")
+
+p15.onmousemove = function(event){
+	p15.style.opacity = 1;
+}
+
+var p16 = document.getElementById("p16")
+
+p16.onmousemove = function(event){
+	p16.style.opacity = 1;
+}
+
+var p17 = document.getElementById("p17")
+
+p17.onmousemove = function(event){
+	p17.style.opacity = 1;
+}
+
+var p18 = document.getElementById("p18")
+
+p18.onmousemove = function(event){
+	p18.style.opacity = 1;
+}
+
+var p19 = document.getElementById("p19")
+
+p19.onmousemove = function(event){
+	p19.style.opacity = 1;
+}
+
+var p20 = document.getElementById("p20")
+
+p20.onmousemove = function(event){
+	p20.style.opacity = 1;
+}
+
+var p21 = document.getElementById("p21")
+
+p21.onmousemove = function(event){
+	p21.style.opacity = 1;
+}
+
+var p22 = document.getElementById("p22")
+
+p22.onmousemove = function(event){
+	p22.style.opacity = 1;
+}
+
+var p23 = document.getElementById("p23")
+
+p23.onmousemove = function(event){
+	p23.style.opacity = 1;
+}
+
+var p24 = document.getElementById("p24")
+
+p24.onmousemove = function(event){
+	p24.style.opacity = 1;
+}
+
+var s3 = document.getElementById("s3")
+
+s3.onmousemove = function(event){
+	s3.style.opacity = 1;
 }
